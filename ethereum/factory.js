9f6c3f8ba790e140c79c0757ce6c5b3898f2b9a6
hosseinsh91 <128283396+hosseinsh91@users.noreply.github.com>
2025-03-07 11:20:24 +0000

    Update factory.js

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0xF5C1350d21D7C73829d0f5976e0882d808cED0C2'
+  ''
 );
 console.log("Factory deployed to:", instance.address);
 
